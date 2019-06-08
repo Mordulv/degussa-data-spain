ef17c525679852bac2f537bf9c95712db0525a6b
Mordulv <50501108+Mordulv@users.noreply.github.com>
2019-06-08 17:28:04 +0200

    Update degussa.py

--- degussa.py
+++ degussa.py
@@ -41,12 +41,57 @@ pdf = PyPDF2.PdfFileReader(pdf_objeto)
 
 txt = open('datos.txt','a')
 
+t = ''
 for i in range(3):
     hoja1 = pdf.getPage(i)
     texto = hoja1.extractText()
     txt.write(texto)
+    t = t +texto
 
 txt.close()
 pdf_objeto.close()
 remove('listadeprecios.pdf')
 
+# Obtiene los datos de un producto determinado
+
+def buscador_producto(texto,codigo):
+    l_cod = len(codigo)
+    l_texto = len(texto)
+    inicio, final, puntero = 0, l_cod, None
+    encontrado = False
+    while final != (l_texto - 1):
+        palabra_aux = texto[inicio:final]
+        if codigo == palabra_aux:
+            print('ENCONTRADO')
+            cont_c = 0
+            txt = ''
+            for i in texto[inicio:]:
+                if cont_c == 2:
+                    return txt
+                else:
+                    txt = txt + i
+                    if txt[len(txt)-1] == '•':
+                        cont_c += 1
+                    
+        else:
+            inicio += 1
+            final += 1
+    print('SIN COINCIDENCIA')
+
+def depurar(codigo,nombre,texto):
+    datos = {}
+    x = lambda x: float(x.replace(' ','').replace(',','.'))
+    aux = texto.split(nombre)
+    aux2 = aux[1].split('•')[0:2]
+    datos['Codigo'] = codigo
+    datos['Nombre'] = nombre
+    dat = list(map(x,aux2))
+    datos['Compra'] = dat[1]
+    datos['Venta'] = dat[0]
+    return datos
+
+# Ejemplo
+texto = buscador_producto(t,'124162/01')
+print(texto)
+y = depurar('124162/01','8 x 1g de oro Maplegram de Canadá',texto)
+print(y)
